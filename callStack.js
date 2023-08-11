4a14937b234e8df836e9d8604973d1237f9aa05f
hoonsbory <tonymkcv@naver.com>
2023-08-11 20:27:56 +0900

    [refactor] add task push log, callstack empty & loop start log

--- callStack.js
+++ callStack.js
@@ -7,40 +7,50 @@ class CallStack extends Stack {
     super();
   }
   push(context) {
-    console.log(`push context! => ${context.name}`);
+    console.log(`Push context! => ${context.name}`);
     super.push(context);
     this.run();
   }
   pop() {
-    console.log(`pop context! => ${this.top.data.name}`);
+    console.log(`Pop context! => ${this.top.data.name}`);
     super.pop();
-    if (this.isEmpty() && !this.eventLoop.isLooping) this.eventLoop.runLoop();
+    if (this.isEmpty() && !this.eventLoop.isLooping) {
+      console.log('callstack is empty. run event loop');
+      this.eventLoop.runLoop();
+    }
   }
   run() {
     const { data } = this.top;
-    console.log(`run context! => ${data.name}`);
+    console.log(`Run context! => ${data.name}`);
 
     data.childFunc?.forEach(context => this.push(context));
+
     if (data.callback)
       switch (data.type) {
         case 'micro':
+          console.log(
+            `Push micro task to micro task queue! => ${data.callback.name}`,
+          );
           this.eventLoop.microtaskQueue.enqueue(data);
           break;
         case 'macro':
+          console.log(`Push setTimeout to web API Thread! => ${data.name}`);
           this.WebAPIThreadPool.webAPITaskEnQueue(data);
           break;
         case 'raf':
+          console.log(
+            `Push raf task to raf task queue! => ${data.callback.name}`,
+          );
           this.eventLoop.rafQueue.enqueue(data.callback);
           break;
         default:
           break;
       }
-
     if (data.resolve)
       this.eventLoop.microtaskQueue.forEach(node => {
         if (node.data.name === data.resolve) {
           node.data.state = 'fullfilled';
-          console.log(`${node.data.name} is fullfilled!`);
+          console.log(`${node.data.name} has fullfilled!`);
         }
       });
     this.pop();
